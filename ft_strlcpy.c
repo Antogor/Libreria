47a314f6d850d9bb1b6483ecf783a8328f0a811c
Antogor <antoniogarzoniniesta@gmail.com>
2019-11-10 21:39:06 +0100

    lcpy2

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 21:56:17 by agarzon-          #+#    #+#             */
-/*   Updated: 2019/11/10 21:30:59 by agarzon-         ###   ########.fr       */
+/*   Updated: 2019/11/10 21:38:51 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,4 @@ size_t		ft_strlcpy(char *dest, const char *src, size_t dstsize)
 	}
 	dest[l] = '\0';
 	return (x);
-	/*if (q < d)
-	{
-		while (l < q)
-		{
-			if (src[l] != '\0')
-				dest[l] = src[l];
-			else if (src[l] == '\0')
-				dest[l] = '\0';
-			l++;
-		}
-	}
-	else
-		return (0);
-	if (l == q)
-		dest[l] = '\0';
-	if (q > x)
-		return (0);
-	return (d);*/
 }
