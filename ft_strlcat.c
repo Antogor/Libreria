6bf27fcd255747899b335381211076d08ba1a960
Antogor <antoniogarzoniniesta@gmail.com>
2019-11-11 17:10:06 +0100

    w

---
+++ ft_strlcat.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/11 09:57:52 by agarzon-          #+#    #+#             */
+/*   Updated: 2019/11/11 09:57:52 by agarzon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t      ft_strlcat(char *dst, const char *src, size_t size)
+{
+    size_t  l;
+    size_t  w;
+    size_t  x;
+    size_t  q;
+
+    l = ft_strlen(src);
+    w = ft_strlen(dst);
+    x = size;
+    q = 0;
+    while (src[q] != '\0' && q < x)
+    {
+        dst[x] = src[q];
+        x++;
+        q++;
+    }
+    dst[x] = '\0';
+    return (l + w);
+}
---
+++ ft_strnstr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/11 10:11:27 by agarzon-          #+#    #+#             */
+/*   Updated: 2019/11/11 10:11:27 by agarzon-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *src, const char *to_find, size_t len)
+{
+	size_t	l;
+
+	l = 0;
+	if (src[l] == '\0')
+		return ((char*)src);
+	while (src[l] != '\0' && l < len)
+	{
+		if (src[l] == to_find[l])
+			return ((char*)to_find);
+		l++;
+	}
+	return (NULL);
+}
