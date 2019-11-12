4fa703fa691593139033cea948df76b6c8f575bd
Antonio Garzon iniesta <agarzon-@c3r9s6.42madrid.com>
2019-11-12 15:46:50 +0100

    calloc finish

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 12:29:59 by agarzon-          #+#    #+#             */
-/*   Updated: 2019/11/12 14:28:12 by agarzon-         ###   ########.fr       */
+/*   Updated: 2019/11/12 15:46:37 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void *cont;
-	size_t l;
-	
-	cont = malloc(size);
-	while (l <= count)
-	{
-		((char*)cont)[l] = '\0';
-		l++;
-	}
+	void	*cont;
+	size_t	l;
+
+	if (count == 0 || size == 0)
+		return (NULL);
+	cont = malloc(count * size);
+	ft_bzero(cont, (count * size));
 	return (cont);
 }
