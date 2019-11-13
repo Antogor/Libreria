ed3e2eb781ad27f8f43e4d06462abe3f8e13afc7
Antonio Garzon iniesta <agarzon-@c3r4s5.42madrid.com>
2019-11-13 12:26:04 +0100

    memchr corregido

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 10:47:45 by agarzon-          #+#    #+#             */
-/*   Updated: 2019/11/09 19:24:02 by agarzon-         ###   ########.fr       */
+/*   Updated: 2019/11/13 12:25:12 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	l = (unsigned char*)s;
 	while (n--)
 	{
-		if (*l == c)
+		if (*l == (unsigned char)c)
 			return (l);
 		l++;
 	}
