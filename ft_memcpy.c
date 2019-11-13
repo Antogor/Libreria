f1ba42e28dd29f2cf04ca94d2c3b636ad52930bb
Antonio Garzon iniesta <agarzon-@c3r4s5.42madrid.com>
2019-11-13 11:24:26 +0100

    memcpy bien

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/07 11:12:52 by agarzon-          #+#    #+#             */
-/*   Updated: 2019/11/09 19:24:21 by agarzon-         ###   ########.fr       */
+/*   Updated: 2019/11/13 11:19:59 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 
 	l = (unsigned char*)dst;
 	q = (const char*)src;
+	if (l == '\0' && q == '\0')
+		return (0);
 	while (n--)
 	{
 		*(l++) = *(q++);
