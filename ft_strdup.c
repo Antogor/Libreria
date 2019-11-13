3f93fa902845994dd18ae6ac9836dfad26db7961
Antonio Garzon iniesta <agarzon-@c3r4s5.42madrid.com>
2019-11-13 10:42:44 +0100

    Norminette

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: agarzon- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 16:50:03 by agarzon-          #+#    #+#             */
-/*   Updated: 2019/11/12 22:22:03 by agarzon-         ###   ########.fr       */
+/*   Updated: 2019/11/13 10:42:28 by agarzon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char *dup;
-	size_t x;
-	
+	char	*dup;
+	size_t	x;
+
 	x = ft_strlen(s1);
 	dup = malloc(x + 1);
 	ft_strlcpy((void*)dup, (void*)s1, x);
